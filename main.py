37a3213ea0d2b87dc57f72e9673783eff4a69f81
Darren Liu <darrrenliu@gmail.com>
2025-10-05 16:45:46 -0400

    decoder basic implementation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from scripts.sim_serial import SimSerial
 from scripts.can_sim import CanMessageSimulator
+from scripts.decoder import Decoder
 
 if __name__ == "__main__":
     sim = SimSerial()
@@ -12,5 +13,7 @@ if __name__ == "__main__":
     for board in boards:
         sim.feed(canSim.gen_datagram(board))
 
+    decoder = Decoder(ser=sim)
+
     while 1:
-        print("READING:", sim.read())
+        decoder.read()
--- scripts/decoder.py
+++ scripts/decoder.py
@@ -1,1 +1,70 @@
-import serial
+from serial import Serial
+from scripts.sim_serial import SimSerial
+
+DATAGRAM_SOF = b'\xaa'
+DATAGRAM_EOF = b'\xbb'
+
+class State:
+    SOF = "SOF"
+    ID = "ID"
+    DLC = "DLC"
+    DATA = "DATA"
+    EOF = "EOM"
+    VALID = "VALID"
+
+class Decoder:
+    def __init__(self, port="/dev/ttyUSB0", baudrate=115200, timeout=1, ser=None):
+        if ser is None:
+            self.ser = Serial(port=port, baudrate=baudrate, timeout=timeout)
+        else:
+            self.ser = ser
+        
+        self.state = State.SOF
+        self.datagram = None
+        self.buffer = []
+
+    def reset_buffer(self):
+        self.buffer = []
+        self.datagram = None
+        self.state = State.SOF
+
+    def read(self):
+        try:
+            byte = self.ser.read(1)[0]
+        except IndexError:
+            return False
+        if self.parse_byte(byte):
+            print(self.datagram)
+        return True
+
+    def parse_byte(self, byte):
+        if self.state == State.SOF or self.state == State.VALID:
+            self.reset_buffer()
+            if byte == 0xAA:
+                self.state = State.ID
+        elif self.state == State.ID:
+            self.buffer.append(byte)
+            if len(self.buffer) == 2:
+                message_id = int.from_bytes(self.buffer, byteorder="big")
+                self.datagram = {"id": message_id}
+                self.buffer = []
+                self.state = State.DLC
+        elif self.state == State.DLC:
+            self.datagram["DLC"] = byte
+            if byte <= 9:
+                self.datagram["DATA"] = []
+                self.state = State.DATA
+            else:
+                self.state = State.SOF
+        elif self.state == State.DATA:
+            self.buffer.append(byte)
+            if len(self.buffer) == self.datagram["DLC"]:
+                self.datagram["DATA"] = self.buffer
+                self.state = State.EOF
+        elif self.state == State.EOF:
+            if byte == 0xBB:
+                self.state = State.VALID
+            else:
+                self.state = State.SOF
+        
+        return self.state == State.VALID
