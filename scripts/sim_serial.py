c1a086f9061022d7e6237223d6be297a69773394
Darren Liu <darrrenliu@gmail.com>
2025-10-04 21:38:53 -0400

    sim_serial class

---
+++ scripts/sim_serial.py
@@ -0,0 +1,36 @@
+import time
+import threading
+from queue import Queue, Empty
+
+class SimSerial:
+
+    def __init__(self):
+        self.rx = Queue()
+        self.is_open = False
+
+    def open(self):
+        self.is_open = True
+
+    def feed(self, data: bytes, paced: bool = False):
+        for b in data:
+            self.rx.put(bytes([b]))
+
+
+    def read(self, size=1):
+        if self.is_open != True:
+            raise Exception("Serial port not open yet...")
+        
+        chunks = []
+
+        for i in range (0, size):
+            try:
+                chunks.append(self.rx.get())
+            except Empty:
+                break
+        
+        out = b"".join(chunks)
+
+        if size > 0:
+            return out[:size]
+        
+        return out
