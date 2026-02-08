fbae6af681b1ae9fbf1fa01bef62de7e8d7fd550
Dana Yuan <danayuan2006@gmail.com>
2026-02-08 13:29:11 -0500

    Create script to receive values from XBee

---
+++ scripts/xbee_read.py
@@ -0,0 +1,17 @@
+from digidevice import xbee
+
+device = xbee.get_device()
+try:
+    device.open()
+
+    def data_receive_callback(xb_msg):
+        print("From %s >> %s" % (xb_msg.remote_device, xb_msg.data.decode()))
+
+    device.add_data_received_callback(data_receive_callback)
+    print("Waiting for data...\n")
+    # Keep the program executing until a key is pressed
+    input()
+finally:
+    if device.is_open():
+        device.close()
+    
